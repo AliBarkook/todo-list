ae754a79295a37ef76c90c2f8ab057ee89385cf8
alibarkook <alibarkook@gmail.com>
2024-01-15 19:19:06 +0330

    feat: add httpclient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withViewTransitions()), provideAnimations(), provideClientHydration()]
+  providers: [provideRouter(routes, withViewTransitions()), provideAnimations(), provideClientHydration(), provideHttpClient()],
+  
 };
